41c2b81fed512d30fd7c4b7f63da2bf6e8624986
mprosperini <maupc9@gmail.com>
2023-07-06 19:44:19 -0400

    first commit

---
+++ src/main/java/com/hotelaluramp/hotelalurachallenge/HotelAluraChallenge.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.hotelaluramp.hotelalurachallenge;
+
+/**
+ *
+ * @author lancer5500ryz
+ */
+public class HotelAluraChallenge {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
